efead9921274ce49b4f30667e53f0c5ea9cc26fb
Nick Heiner <nick.heiner@opower.com>
2013-05-17 16:29:49 -0400

    adding documentation of what will be in message as well as guacamole handler

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 import os
+import re
 from flask import Flask, request, json
 
 app = Flask(__name__)
@@ -8,4 +9,35 @@ app.config['DEBUG'] = True
 @app.route('/')
 def listen():
   message = json.loads(request.args.get('message', ''))
-  return 'Echo: ' + json.dumps(message)
+
+  response = guacamole(message)
+
+  if response is not None:
+    return response
+
+  return ''
+
+"""
+`message` will be of the following form:
+
+{
+  "text": "how bout u", 
+  "id": 927874479, 
+  "room": 382086, 
+  "user": {
+    "name": "Nick Heiner", 
+    "email_address": "nick.heiner@opower.com", 
+    "room": 382086, 
+    "admin": false, 
+    "id": 1284158, 
+    "avatar_url": "http://dge9rmgqjs8m1.cloudfront.net/global/447cd8c718ec08087db7b11971313dff6b97f7832951b1815b766b238a8774b7e37be1e457d1e9199d969e9ce216b5b730d329452b8599399ef574257ed0cff091a49fb43115e0627eb235e5d7768835/avatar.gif?r=3&quot;", 
+    "type": "Member", 
+    "created_at": "2012/10/04 14:55:41 0000"
+    }, 
+  "done": false
+}
+"""
+
+def guacamole(message):
+  if re.findall('guacamole', message['text'], re.I):
+    return 'hey did you that Guarapo is 7th most famous guacamole bar in the mid-atlantic region?'
