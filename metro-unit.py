eb005124f04c2cd5411bfef6147efc9e0e7f25ef
Nick Heiner <nick.heiner@opower.com>
2013-05-20 18:15:17 -0400

    using fixtures instead of inline data; adding more unit tests that have data in a different order

--- metro-unit.py
+++ metro-unit.py
@@ -5,80 +5,22 @@ import json
 class TestSequenceFunctions(unittest.TestCase):
 
     def setUp(self):
-      self.response = """
-    {
-    "Trains": [{
-        "Car": "6",
-        "Destination": "NewCrltn",
-        "DestinationCode": "D13",
-        "DestinationName": "New Carrollton",
-        "Group": "1",
-        "Line": "OR",
-        "LocationCode": "K01",
-        "LocationName": "Court House",
-        "Min": "1"
-    }, {
-        "Car": "6",
-        "Destination": "Vienna",
-        "DestinationCode": "K08",
-        "DestinationName": "Vienna",
-        "Group": "2",
-        "Line": "OR",
-        "LocationCode": "K01",
-        "LocationName": "Court House",
-        "Min": "2"
-    }, {
-        "Car": "8",
-        "Destination": "NewCrltn",
-        "DestinationCode": "D13",
-        "DestinationName": "New Carrollton",
-        "Group": "1",
-        "Line": "OR",
-        "LocationCode": "K01",
-        "LocationName": "Court House",
-        "Min": "8"
-    }, {
-        "Car": "8",
-        "Destination": "Vienna",
-        "DestinationCode": "K08",
-        "DestinationName": "Vienna",
-        "Group": "2",
-        "Line": "OR",
-        "LocationCode": "K01",
-        "LocationName": "Court House",
-        "Min": "9"
-    }, {
-        "Car": "6",
-        "Destination": "Vienna",
-        "DestinationCode": "K08",
-        "DestinationName": "Vienna",
-        "Group": "2",
-        "Line": "OR",
-        "LocationCode": "K01",
-        "LocationName": "Court House",
-        "Min": "19"
-    }, {
-        "Car": "6",
-        "Destination": "Largo",
-        "DestinationCode": "G05",
-        "DestinationName": "Largo Town Center",
-        "Group": "1",
-        "Line": "OR",
-        "LocationCode": "K01",
-        "LocationName": "Court House",
-        "Min": ""
-    }]
-}
-"""
+      self.orderedMetroData = json.loads(open('fixtures/ordered.json', 'r').read())
+      self.unorderedMetroData = json.loads(open('fixtures/unordered.json', 'r').read())
 
-    def test_timeForNextWestTrain(self):
-        metroData = json.loads(self.response)
-        self.assertEqual("2", metro.timeForNextWestTrain(metroData))
-    
-    def test_timeForNextEastTrain(self):
-        metroData = json.loads(self.response)
-        self.assertEqual("1", metro.timeForNextEastTrain(metroData))
+    def test_ordered_timeForNextWestTrain(self):
+        self.assertEqual("2", metro.timeForNextWestTrain(self.orderedMetroData))
+
+    def test_ordered_timeForNextEastTrain(self):
+        self.assertEqual("1", metro.timeForNextEastTrain(self.orderedMetroData))
+
+    def test_unordered_timeForNextWestTrain(self):
+        self.assertEqual("2", metro.timeForNextWestTrain(self.unorderedMetroData))
+
+    def test_unordered_timeForNextEastTrain(self):
+        self.assertEqual("1", metro.timeForNextEastTrain(self.unorderedMetroData))
 
 if __name__ == '__main__':
     unittest.main()
+
 
--- metro.py
+++ metro.py
@@ -1,8 +1,12 @@
+
+import urllib2
+
 """
   Return the WMATA json response for the Courthouse metro stop
 """
 def getJsonForCourthouse():
-  pass
+  function = urllib2.urlopen('http://api.wmata.com/StationPrediction.svc/json/GetPrediction/K01?api_key=ten3y6u8f6qdn5trz7am72jq')
+  return function.read()
 
 """
 Argument: the return value of getJsonForCourthouse()
@@ -17,27 +21,33 @@ def timeForNextWestTrain(wmataResponse):
 
   Return the time until the next train leaves Eastwarn (towards Rosslyn / New Carrolton) (in minutes, as a string)
 """
+    
+  n_trains = len(wmataResponse['Trains'])
+  if n_trains < 1:
+    return ''  #returns None when there is no info about trains
+  
+  i = 0 #not sure if i need this, check later
+  west_trains = []
+  for i in range(0, n_trains):
+    if wmataResponse['Trains'][i]['Destination'] == "Vienna":
+        west_trains.append(i)
+    i =+ i
+  minutes = [int(wmataResponse['Trains'][n]['Min']) for n in west_trains]
+  min_minutes = min(minutes)
+  return `min_minutes`
 
-  return ((wmataResponse['Trains'])[1])['Min']
-  
 
+def timeForNextEastTrain(wmataResponse):  
+  n_trains = len(wmataResponse['Trains'])
+  if n_trains < 1:
+    return ''  #returns None when there is no info about trains
   
-  
-
-def timeForNextEastTrain(wmataResponse):
-  #RASCUNHO QUE FUNCIONA
-  
-  #print `wmataResponse`
-  ##print 'hi'
-  ##print wmataResponse['Trains']
-  ##print 'hello'
-  #inside_dict = wmataResponse['Trains']
-  #first_element = inside_dict[0]
-  ##print first_element
-  ##print 'oi'
-  #minutes = first_element['Min']
-  #
-  ##print minutes
-  #return minutes
-  
-    return ((wmataResponse['Trains'])[0])['Min']
+  i = 0 #not sure if i need this, check later
+  east_trains = []
+  for i in range(0, n_trains):
+    if wmataResponse['Trains'][i]['Destination'] == "NewCrltn":
+        east_trains.append(i)
+    i =+ i
+  minutes = [int(wmataResponse['Trains'][n]['Min']) for n in east_trains]
+  min_minutes = min(minutes)
+  return `min_minutes`
