86e7a636e8e6bd31cedebb78c09073464e583068
Helena Wu <helenawu.hw@gmail.com>
2013-05-21 15:30:24 -0400

    changed functions and unit tests
    created test.json and test2.json
    changed functions to look for trains to nc, vienna and largo
    added unit tests

--- metro.py
+++ metro.py
@@ -1,4 +1,5 @@
 
+
 import urllib2
 
 """
@@ -9,36 +10,54 @@ def getJsonForCourthouse():
   return function.read()
 
 
-
-def time(response, direction, destination):
+"""
+  Return the minimum time for the next train to destination (Vienna, New Carrolton or Largo) to arrive in a string
+"""
+def time(response, destination):
+  list_indicies = []
   n_trains = len(response['Trains'])
   if n_trains < 1:
     return ''  #returns None when there is no info about trains
   for i in range(0, n_trains):
     if response['Trains'][i]['Destination'] == destination:
-        direction.append(i)
-  return `min([int(response['Trains'][n]['Min']) for n in direction])`
+        list_indicies.append(i)
+  minutes_list = [response['Trains'][n]['Min'] for n in list_indicies if response['Trains'][n]['Min'] != '']
+  if len(minutes_list) > 1:
+    int_list = [int(x) for x in minutes_list]
+    return `min(int_list)`
+  else:
+    return ''
+
 
 """
-Argument: the return value of getJsonForCourthouse()
+  Argument: the return value of getJsonForCourthouse()
 
   Return the time until the next train leaves Westward (towards Clarendon / Vienna) (in minutes, as a string)
 """
+def timeForNextTrainToV(wmataResponse):
+  return time(wmataResponse, "Vienna")
 
 
-def timeForNextWestTrain(wmataResponse):
-  """
+"""
   Argument: the return value of getJsonForCourthouse()
 
   Return the time until the next train leaves Eastwarn (towards Rosslyn / New Carrolton) (in minutes, as a string)
 """
-  west_trains = []
-  return time(wmataResponse, west_trains, "Vienna")
+def timeForNextTrainToNC(wmataResponse):
+  return time(wmataResponse, "NewCrltn")
 
 
-def timeForNextEastTrain(wmataResponse):
-  east_trains = []
-  return time(wmataResponse, east_trains, "NewCrltn")
+"""
+  Argument: the return value of getJsonForCourthouse()
 
+  Return the time until the next train leaves Eastwarn (towards Rosslyn / Largo Town Center) (in minutes, as a string)
+  if no trains to Largo, return "No trains to Largo"
+"""
+def timeForNextTrainToL(wmataResponse):
+  if time(wmataResponse, "Largo"):
+    return time(wmataResponse, "Largo")
+  else:
+    return "No trains to Largo"
+  
 
 
