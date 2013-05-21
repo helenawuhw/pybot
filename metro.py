867d9cf74a455edbf6b00188625e8066ee78abe8
Helena Wu <helenawu.hw@gmail.com>
2013-05-21 11:17:54 -0400

    review comments

--- metro.py
+++ metro.py
@@ -8,6 +8,17 @@ def getJsonForCourthouse():
   function = urllib2.urlopen('http://api.wmata.com/StationPrediction.svc/json/GetPrediction/K01?api_key=ten3y6u8f6qdn5trz7am72jq')
   return function.read()
 
+
+
+def time(response, direction, destination):
+  n_trains = len(response['Trains'])
+  if n_trains < 1:
+    return ''  #returns None when there is no info about trains
+  for i in range(0, n_trains):
+    if response['Trains'][i]['Destination'] == destination:
+        direction.append(i)
+  return `min([int(response['Trains'][n]['Min']) for n in direction])`
+
 """
 Argument: the return value of getJsonForCourthouse()
 
@@ -21,33 +32,13 @@ def timeForNextWestTrain(wmataResponse):
 
   Return the time until the next train leaves Eastwarn (towards Rosslyn / New Carrolton) (in minutes, as a string)
 """
-
-  n_trains = len(wmataResponse['Trains'])
-  if n_trains < 1:
-    return ''  #returns None when there is no info about trains
-  
-  i = 0 #not sure if i need this, check later
   west_trains = []
-  for i in range(0, n_trains):
-    if wmataResponse['Trains'][i]['Destination'] == "Vienna":
-        west_trains.append(i)
-    i =+ i
-  minutes = [int(wmataResponse['Trains'][n]['Min']) for n in west_trains]
-  min_minutes = min(minutes)
-  return `min_minutes`
+  return time(wmataResponse, west_trains, "Vienna")
 
 
-def timeForNextEastTrain(wmataResponse):  
-  n_trains = len(wmataResponse['Trains'])
-  if n_trains < 1:
-    return ''  #returns None when there is no info about trains
-  
-  i = 0 #not sure if i need this, check later
+def timeForNextEastTrain(wmataResponse):
   east_trains = []
-  for i in range(0, n_trains):
-    if wmataResponse['Trains'][i]['Destination'] == "NewCrltn":
-        east_trains.append(i)
-    i =+ i
-  minutes = [int(wmataResponse['Trains'][n]['Min']) for n in east_trains]
-  min_minutes = min(minutes)
-  return `min_minutes`
+  return time(wmataResponse, east_trains, "NewCrltn")
+
+
+
