9774c41a6cb214fa7ede73f6a024fa167ab6d361
Helena Wu <helenawu.hw@gmail.com>
2013-05-23 19:51:24 -0400

    implemented stubs

--- bomb-unit.py
+++ bomb-unit.py
@@ -9,22 +9,22 @@ class TestSequenceFunctions(unittest.TestCase):
         self.getSizesResponse = json.loads(open('fixtures/bomb-get-sizes.json', 'r').read())
 
     def test_shouldRespond_emptyString(self):
-        self.assertEquals(False, bomb.shouldRespond(''))
+        self.assertEquals((None, None), bomb.shouldRespond(''))
 
     def test_shouldRespond_notRelevant(self):
-        self.assertEquals(False, bomb.shouldRespond('obot is going to the moon'))
+        self.assertEquals((None, None), bomb.shouldRespond('obot is going to the moon'))
 
     def test_shouldRespond_noTopic(self):
-        self.assertEquals((bomb.DEFAULT_BOMB_TOPIC, 10), bomb.shouldRespond('obot bomb 10'))
+        self.assertEquals((bomb.DEFAULT_BOMB_TOPIC, "10"), bomb.shouldRespond('obot bomb 10'))
 
     def test_shouldRespond_noCount(self):
         self.assertEquals(('dangerzone', bomb.DEFAULT_BOMB_COUNT), bomb.shouldRespond('obot dangerzone bomb'))
 
     def test_shouldRespond_countAndTopic(self):
-        self.assertEquals(('loggins', 8), bomb.shouldRespond('obot loggins bomb 8'))
+        self.assertEquals(('loggins', "8"), bomb.shouldRespond('obot loggins bomb 8'))
 
     def test_shouldRespond_topicWithSpaces(self):
-        self.assertEquals(('danger zone', 8), bomb.shouldRespond('obot danger zone bomb 8'))
+        self.assertEquals(('danger zone', "8"), bomb.shouldRespond('obot danger zone bomb 8'))
 
     def test_parsePhotoIdsFromJsonResponse(self):
         self.assertEquals(
@@ -35,7 +35,7 @@ class TestSequenceFunctions(unittest.TestCase):
     def test_parsePhotoUrlFromJsonResponse(self):
         self.assertEquals(
           # Feel free to change this expected response to something that makes more sense
-          'http:\/\/farm9.staticflickr.com\/8120\/8792081739_9538871ce5_c.jpg',
+          r'http://farm9.staticflickr.com/8120/8792081739_9538871ce5.jpg',
           bomb.parsePhotoUrlFromJsonResponse(self.getSizesResponse)
         )
 
--- bomb.py
+++ bomb.py
@@ -1,12 +1,22 @@
+import urllib2
+import re
+import json
+import random
+
 """
 Custom photo bombing via the flickr api
 """
+
 
 """
   Return a response for `text` if we want to respond; otherwise None
 """
 def handleMessage(text):
-  pass
+  s = shouldRespond(text)
+  if s != (None, None):
+    topic, number = s
+    list_ids = getPhotoIdsForTopic(topic)
+    return ','.join([getPhotoUrlForId(photo_id) for photo_id in list_ids])
 
 
 """
@@ -31,40 +41,64 @@ DEFAULT_BOMB_TOPIC = 'kenny loggins'
     shouldRespond('obot bomb 13') == (DEFAULT_BOMB_TOPIC, 13)
 """
 def shouldRespond(text):
-  pass
+  match = re.search(r'obot ((\w* )*)bomb( (\d*))?', text)
+  if match is not None:
+    topic = match.group(1)
+    number = match.group(3)
+    if number is None:
+      return topic.strip(), DEFAULT_BOMB_COUNT
+    if topic.strip() == '':
+      return DEFAULT_BOMB_TOPIC, number.strip()
+    if topic.strip() == '' and number is None:
+      return DEFAULT_BOMB_TOPIC, DEFAULT_BOMB_COUNT
+    else:
+      return topic.strip(), number.strip()
+  else:
+    return None, None
+
 
 """
   Call the flickr API and return a list of photo ids corresponding to a specific topic
 """
 def getPhotoIdsForTopic(topic):
-  pass
+  return parsePhotoIdsFromJsonResponse(getPhotoIdJsonForTopic(topic))
+
 
 """
   Call the flickr API and return the json response for a photo search on a topic
 """
 def getPhotoIdJsonForTopic(topic):
-  pass
+  urled_topic = urllib2.quote(topic)
+  apiCall = urllib2.urlopen('http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=767cf3bb0a87cfafaf46bbebda08e646&tags=' + urled_topic + '&format=json&nojsoncallback=1')
+  return json.loads(apiCall.read())
+
 
 """
   Parse the json response to determine the list of ids for the photos
 """
 def parsePhotoIdsFromJsonResponse(response):
-  pass
+  return [photo['id'] for photo in response['photos']['photo']]
+
 
 """
   Call the flickr API to get the image url for a photo id
 """
-def getPhotoUrlForId(id):
-  pass
+def getPhotoUrlForId(id_number):
+  return parsePhotoUrlFromJsonResponse(getPhotoUrlJsonForId(id_number))
+ 
 
 """
-  Call the flickr API to get the photo url response for a specific photo id
+  Call the flickr API to get the json photo url response for a specific photo id
 """
-def getPhotoUrlJsonForId(id):
-  pass
+def getPhotoUrlJsonForId(id_number):
+  apiCall = urllib2.urlopen('http://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=8ac641379dc965b3ffb0eb1ed4bf055a&photo_id=' + id_number + '&format=json&nojsoncallback=1')
+  return json.loads(apiCall.read())
+
 
 """
   Parse the json response to figure out the right photo url
 """
 def parsePhotoUrlFromJsonResponse(response):
-  pass
+  n = (len(response['sizes']['size']))
+  return response['sizes']['size'][n/2]['source']
+  
