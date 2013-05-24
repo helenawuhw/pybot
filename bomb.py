90d23591077ac7dc4fe43c32b75c2d95eaa9fa95
Helena Wu <helenawu.hw@gmail.com>
2013-05-24 11:16:25 -0400

    changed to the right API key

--- bomb.py
+++ bomb.py
@@ -69,7 +69,7 @@ def getPhotoIdsForTopic(topic):
 """
 def getPhotoIdJsonForTopic(topic):
   urled_topic = urllib2.quote(topic)
-  apiCall = urllib2.urlopen('http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=767cf3bb0a87cfafaf46bbebda08e646&tags=' + urled_topic + '&format=json&nojsoncallback=1')
+  apiCall = urllib2.urlopen('http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=8ac641379dc965b3ffb0eb1ed4bf055a&tags=' + urled_topic + '&format=json&nojsoncallback=1')
   return json.loads(apiCall.read())
 
 
