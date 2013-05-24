404cace910e3b8433d827f7e311671fef88b2734
Helena Wu <helenawu.hw@gmail.com>
2013-05-24 17:40:44 -0400

    made it return something when nothing is found

--- bomb.py
+++ bomb.py
@@ -15,6 +15,8 @@ def handleMessage(text):
   topic, number = shouldRespond(text)
   if (topic, number) != (None, None):
     list_ids = getPhotoIdsForTopic(topic, number)
+    if list_ids is None:
+      return "Couldn't find anything to bomb :("
     return [getPhotoUrlForId(photo_id) for photo_id in list_ids]
 
 
